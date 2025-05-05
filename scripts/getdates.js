be97a6b25e495bb84038873a570abc89bb8ef9f3
adavidarevalo <davidarevaloc20@gmail.com>
2025-05-05 01:51:11 -0600

    Initial setup: Add homepage with profile, resources, and styling

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Get current year for the copyright
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+// Get the last modified date of the document
+document.getElementById('lastModified').textContent = `Last Modified: ${document.lastModified}`;
